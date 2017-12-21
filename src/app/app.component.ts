e01d24679d7edfd0f10a53231182b7b34415ae1e
angel munoz <angelmsanchez85@gmail.com>
2017-12-21 09:57:17 +0100

    -add scss to angular cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-angular',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
 
-  constructor(private router: Router) {
-  }
-
-  goTo(route: string): void {
-    console.log('goto ' + route);
-    this.router.navigate([route]);
-  }
 }
--- src/app/map/components/map.component.ts
+++ src/app/map/components/map.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
 
 export class MapComponent implements OnInit {
